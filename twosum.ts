10254997910df5a469117d054beb82f045a0f339
Nick Chiong <njcolaschiong@gmail.com>
2023-02-13 23:53:15 +0800

    Two Sum

---
+++ twosum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    let map: Map<number, number> = new Map();
+    for(let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+};
